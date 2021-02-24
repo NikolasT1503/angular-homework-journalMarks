d720a64e0c469ff81677c0c6e95ee201c1fc4233
NikolasT1503 <Nikolas_t@mail.ru>
2021-02-24 23:06:15 +0300

    add lessons component, первичная разметка html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import lessonsData from '../assets/lessons_data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-homework-journalMarks';
+
+  lessons: { id: string; dateLesson: string; lesson: string; theme: string; homework: string; note: string; }[];
+  
+  constructor(){
+    //console.log(lessonsData[0].dateLesson);
+    this.lessons = lessonsData;
+  }
+
 }
--- src/app/journal/lessons/lessons.component.ts
+++ src/app/journal/lessons/lessons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lessons',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonsComponent implements OnInit {
 
-  constructor() { }
+  @Input() lessonIn;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
